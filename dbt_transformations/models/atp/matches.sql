29c4613f6d300c9a21fb5675fc64a953f022e97b
mposada97 <mateo.posada1997@gmail.com>
2025-10-18 14:58:22 -0500

    added matches model

---
+++ dbt_transformations/models/atp/matches.sql
@@ -0,0 +1,20 @@
+{{config(
+    materialized='external',
+    location='output/matches.parquet',
+    format='parquet'
+)}}
+
+WITH noWinLoss AS (
+    SELECT COLUMNS(col ->
+        NOT regexp_matches(col, 'w_.*') AND 
+        NOT regexp_matches(col, 'L_.*')
+    ) -- this selects all columns that dont start with w_ or l_
+    FROM {{ source('github', 'matches_file') }}
+)
+
+SELECT * REPLACE (
+    cast(strptime(tourney_date, '%Y%m%d') AS date) as tourney_date
+)
+FROM noWinLoss -- this grabs all columns from out noWinLoss CTE but it replaces the tourney_date by our formatted tourney_date in place
+
+
